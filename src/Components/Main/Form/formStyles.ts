fda5370a9b1b1d38d994469707ac02bf5e85277b
nikhildhage <nikhil.dhage@gmail.com>
2024-08-14 22:55:42 -0700

    Form component styles

---
+++ src/Components/Main/Form/formStyles.ts
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+	radioGroup: {
+		display: 'flex',
+		justifyContent: 'center',
+		marginBottom: '-10px',
+	},
+	button: {
+		marginTop: '20px',
+		backgroundColor: 'skyblue',
+		color: 'white',
+	},
+	grid: {
+		//paddingTop: 0,
+		//paddingLeft: "20px",
+		//paddingRight: "20px",
+	},
+}));
