53012e5fe99c9143c7a7d18334f2c1cc6c20a8d1
nikhildhage <nikhil.dhage@gmail.com>
2024-10-10 19:30:30 -0400

    Changed vite settings not auto open

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		open: 'main.tsx',
-	},
+	server: {},
 	build: {
 		minify: 'terser',
 		sourcemap: true,
