219aa9f22972859764e0a08c3a606bbf6af282b5
nikhildhage <nikhil.dhage@gmail.com>
2026-01-07 17:44:53 -0500

    Added styles for BorderTop and othrs styles

--- src/Components/Details/detailStyles.ts
+++ src/Components/Details/detailStyles.ts
@@ -1,21 +1,20 @@
 import { makeStyles } from "@mui/styles";
 import { BorderBottom } from '@mui/icons-material';
-import { grey } from "@mui/material/colors";
 
 export default makeStyles(() => ({
 	income: {
-		border: '3px solid rgba(0, 0, 0, 0.2)',
-		borderBottom: '8px solid rgba(0, 255, 0, 0.5)',
-		backgroundColor: " #5c5959  ",
+		border: '3px solid  rgba(0, 0, 0, 0.2)',
+		borderTop: '35px solid  rgb(14, 14, 238)',
+		backgroundColor: "#f8f8f8",
 		marginLeft: '-20px',
 		shadows: '24px 24px 24px 24px rgba(0, 0, 0, 1)',
 	},
 
 	expense: {
 		border: '3px solid rgba(0, 0, 0, 0.2)',
-		borderBottom: '8px solid rgba(255, 0, 0, 0.5)',
+		borderTop: '35px solid rgb(14, 14, 238)',
 		backgroundColor: " #f8f8f8 ",
 		marginLeft: '-20px',
-		shadows: 'none'
+		shadows: '24px 24px 24px 24px rgba(0, 0, 0, 1)',
 	},
 }));
