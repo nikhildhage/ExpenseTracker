3657e9b02a59b3f46c0c96aeedde4f79527d2337
nikhildhage <nikhil.dhage@gmail.com>
2026-02-16 19:46:28 -0500

    Removed borderr top style

--- src/Components/Details/detailStyles.ts
+++ src/Components/Details/detailStyles.ts
@@ -1,19 +1,17 @@
-import { makeStyles } from "@mui/styles";
+import { makeStyles } from '@mui/styles';
 import { BorderBottom } from '@mui/icons-material';
 
 export default makeStyles(() => ({
 	income: {
 		border: '3px solid  rgba(0, 0, 0, 0.2)',
-		borderTop: '35px solid  rgb(14, 14, 238)',
-		backgroundColor: "#f8f8f8",
+		backgroundColor: '#f8f8f8',
 		marginLeft: '-20px',
 		shadows: '24px 24px 24px 24px rgba(0, 0, 0, 1)',
 	},
 
 	expense: {
 		border: '3px solid rgba(0, 0, 0, 0.2)',
-		borderTop: '35px solid rgb(14, 14, 238)',
-		backgroundColor: " #f8f8f8 ",
+		backgroundColor: ' #f8f8f8 ',
 		marginLeft: '-20px',
 		shadows: '24px 24px 24px 24px rgba(0, 0, 0, 1)',
 	},
